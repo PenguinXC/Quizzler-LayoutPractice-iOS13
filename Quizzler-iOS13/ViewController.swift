05b53656ad4bd67d08b530840193488916d7a977
PengiunXC <113483621+PenguinXC@users.noreply.github.com>
2025-04-26 17:53:45 +0700

    96. Setting up the Quizzler Project and Showing the Questions

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        "Four + Two = Six",
+        "Five - Three > One ",
+        "Three + Eight < Ten",
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+//        progressBar.progress = Float(questionNumber) / Float(quiz.count)
+//        
+//        if questionNumber == quiz.count - 1 {
+//            trueButton.isEnabled = false
+//            falseButton.isEnabled = false
+//        } else {
+//            trueButton.isEnabled = true
+//            falseButton.isEnabled = true
+//        }
+    }
+    
 }
 
